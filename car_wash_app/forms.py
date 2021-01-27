6ee18a25c4fd4f1abfd7db9ac763d249872fee4f
mariami <mari.kifshidze@gmail.com>
2021-01-27 16:09:49 +0400

    making forms for profile and user update

---
+++ car_wash_app/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import CompanyProfile
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', "email"]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CompanyProfile
+        fields = ["image"]
