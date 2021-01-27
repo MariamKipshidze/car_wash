4c2ce8181a9fb07656b56a138a32f1db6289689e
mariami <mari.kifshidze@gmail.com>
2021-01-27 15:10:32 +0400

    making views and configuring urls.py and templates

--- car_wash_app/urls.py
+++ car_wash_app/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from . import views as user_views
+from .views import home, detail
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    
+    path("", user_views.home, name = "home" ),
+    path('detail/<int:pk>/', user_views.detail, name = "branch-detail"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- car_wash_app/views.py
+++ car_wash_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Branch
 
-# Create your views here.
+
+def home(request):
+    context = {}
+    branches = Branch.objects.all()
+    context["branches"] = branches
+
+    return render(request, "car_wash_app/home.html", context)
+
+
+def detail(request, pk):
+    branch = get_object_or_404(Branch, id=pk)
+    return render(request, 'car_wash_app/branch_detail.html', context={
+        'branch': branch
+    })
