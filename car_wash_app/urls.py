b932a625252ecc0077bb3353038c2c12bb4511b5
mariami <mari.kifshidze@gmail.com>
2021-01-27 16:26:54 +0400

    making profile view (adding profile.html and configuring base.html, urls.py and settigns.py)

--- car_wash_app/urls.py
+++ car_wash_app/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views as user_views
-from .views import home, detail
+from django.contrib.auth import views as auth_views 
+from .views import home, detail, profile
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path("", user_views.home, name = "home" ),
+    path("profile/", user_views.profile, name = "profile"),
+    path("login/", auth_views.LoginView.as_view(template_name  = "car_wash_app/login.html"), name = "login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name = "car_wash_app/logout.html"), name = "logout"),
     path('detail/<int:pk>/', user_views.detail, name = "branch-detail"),
 ]
 
--- car_wash_app/views.py
+++ car_wash_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Branch
+from .forms import ProfileUpdateForm, UserUpdateForm
 
 
 def home(request):
@@ -17,3 +19,26 @@ def detail(request, pk):
     return render(request, 'car_wash_app/branch_detail.html', context={
         'branch': branch
     })
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"your account has been updated!")
+            return redirect("profile")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+
+    context = {
+        "u_form": u_form,
+        "p_form": p_form
+    }
+
+    return render(request, "car_wash_app/profile.html", context)
