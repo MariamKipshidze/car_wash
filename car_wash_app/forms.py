c36f15545dfd886e8c6fccb6006ab72698b3d8fd
mariami <mari.kifshidze@gmail.com>
2021-01-28 11:49:11 +0400

    adding order form

--- car_wash_app/forms.py
+++ car_wash_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import CompanyProfile
+from .models import CompanyProfile, Order
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -10,7 +10,14 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', "email"]
 
+
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = CompanyProfile
         fields = ["image"]
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('order_date', 'employee', 'car',)
