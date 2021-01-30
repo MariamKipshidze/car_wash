004e7b3afe2fb6ef80b2887ef348cbc7898df34c
mariami <mari.kifshidze@gmail.com>
2021-01-30 16:23:03 +0400

    adding EmployeeRegisterForm and EmployeeProfileRegisterForm

--- car_wash_app/forms.py
+++ car_wash_app/forms.py
@@ -1,14 +1,29 @@
 from django import forms
 from user.models import User
-from .models import CompanyProfile, Order
+from .models import CompanyProfile, Order, EmployeeProfile
 from django.contrib.auth.forms import UserCreationForm
 
 
 class CompanyRegisterForm(UserCreationForm):
-
     class Meta:
         model = User
         fields = [ "email", "password1", "password2"]
+
+
+class EmployeeRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [ "email", "status", "password1", "password2",]
+
+
+class EmployeeProfileRegisterForm(forms.ModelForm):
+    def __init__(self, current_company, *args, **kwargs):
+        super(EmployeeProfileRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['branch'].queryset = self.fields['branch'].queryset.filter(company = current_company)
+
+    class Meta:
+        model = EmployeeProfile
+        fields = ["employee", "full_name", "age", "mobile_number", "manager", "branch", "salary",]
 
 
 class UserUpdateForm(forms.ModelForm):
