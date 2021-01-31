ba449174074c22f50d2fc40d694a7d54d08427c1
mariami <mari.kifshidze@gmail.com>
2021-01-31 20:01:16 +0400

    configuring

--- car_wash_app/models.py
+++ car_wash_app/models.py
@@ -23,7 +23,7 @@ class CompanyProfile(models.Model):
     mobile_number = models.CharField(max_length=20, verbose_name=_("Mobile Number"),null = True, blank = True)
 
     def __str__(self):
-        return f"{self.name} Profile"
+        return f'{self.name}'
 
     def save(self, *args, **kwargs):
         super(CompanyProfile, self).save(*args, **kwargs)
