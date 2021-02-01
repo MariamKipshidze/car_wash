90072f06513cb7f7ef0108aa19e984717a4b92b1
mariami <mari.kifshidze@gmail.com>
2021-02-01 13:19:39 +0400

    adding company register path

--- car_wash_app/urls.py
+++ car_wash_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views as user_views
 from django.contrib.auth import views as auth_views 
 from .views import home, detail, profile
-from .views import employee_profile, employee_register
+from .views import employee_profile, employee_register, company_register
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -15,6 +15,7 @@ urlpatterns = [
     path('detail/<int:pk>/', user_views.detail, name = "branch-detail"),
     path('employee/detail/<int:pk>/', user_views.employee_profile, name = "employee-detail"),
     path('employee/register/<int:pk>', user_views.employee_register, name = "employee-register"),
+    path('company/register/', user_views.company_register, name = "company-register"),
 ]
 
 if settings.DEBUG:
