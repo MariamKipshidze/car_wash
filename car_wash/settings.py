c432ac8aabbd0d99f564a2567120d5ce13f20a10
mariami <mari.kifshidze@gmail.com>
2021-02-13 13:35:01 +0400

    configuring media root

--- car_wash/settings.py
+++ car_wash/settings.py
@@ -122,9 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import os
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = "media"
 MEDIA_URL = "/media/"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
